c2cc436f3fc0db3512b68d8d9535e369df07aea3
nivetha-techie <nivethabala2021@gmail.com>
2026-02-18 12:12:07 +0530

    first code of intellij

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+            int a=10;
+            int b=20;
+            int c=a+b;
+             System.out.println(c);
+    }
+}
