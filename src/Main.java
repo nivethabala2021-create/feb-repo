4446588cce208b2ec74d6a946d8eca7cfcfb9c51
nivetha-techie <nivethabala2021@gmail.com>
2026-02-18 13:10:01 +0530

    modifying statement in output

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,6 @@ public class Main {
             int a=10;
             int b=20;
             int c=a+b;
-             System.out.println(c);
+             System.out.println("the output of c is "+c);
     }
 }
